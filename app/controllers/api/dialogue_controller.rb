f37930b71da04a2585274b26bc7b13a67a28a0b3
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-02-03 17:45:28 +1200

    update dialogue controller to handle quotes for team as well

--- app/controllers/api/dialogue_controller.rb
+++ app/controllers/api/dialogue_controller.rb
@@ -1,20 +1,41 @@
 require_relative "../../data/driver_dialogue"
+require_relative "../../data/team_dialogue"
+
 
 class Api::DialogueController < ApplicationController
+  def driver
+    driver = params[:driver]
+    driver = driver.downcase.strip.gsub(" ", "_")   # trying to match as best as possible   
+    dialogue = DriverDialogue.random_for(driver)
 
-    def show
-        driver = params[:driver]
-        dialogue = DriverDialogue.random_for(driver)
+    if dialogue
+      render json: { driver: driver, dialogue: dialogue }
+    else
+      render json: { 
+        error: "No dialogue found for driver #{driver}" ,
+        available_drivers: DriverDialogue.drivers
+      }, status: :not_found
+    end
+  end
 
-        if dialogue
-            render json: { driver: driver, dialogue: dialogue }
-        else
-            render json: { 
-                error: "No dialogue found for driver #{driver}" ,
-                available_drivers: DriverDialogue.drivers
-            }, status: :not_found
-        end
+  def team
+    team = params[:team]
+    team = team.downcase.strip.gsub(" ", "_") # trying to match as best as possible
+    dialogue = TeamDialogue.get_dialogue(team)
 
-    end 
+    if dialogue
+      render json: { team: team, dialogue: dialogue }
+    else
+      render json: { 
+        error: "No dialogue found for team #{team}",
+        available_teams: TeamDialogue.teams
+      }, status: :not_found
+    end
+  end
+
+  private
+
+  def not_found(type)
+    render json: { error: "#{type} not found" }, status: 404
+  end
 end
-
