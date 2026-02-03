66e7c5737cce9000105a8d6377cb0f05806c21bc
Jena Khatri <188423494+Khatri-That-codes@users.noreply.github.com>
2026-02-03 17:13:12 +1200

    update files relating to use of active record and database

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module F1RadioApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.orm :none
+    end
   end
 end
